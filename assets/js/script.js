81b684f1403fbaf183556e3bae92d871d646d7a6
Kate McColl <kate@katemccollart.com>
2021-04-08 20:44:32 +1000

    Fix: Hero Container

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,35 +55,24 @@ function heroLocator(event) {
     }
 
     // See https://developer.marvel.com/docs#!/public/getCreatorCollection_get_0
-    fetchJsonData(buildApiUrl("characters") + `&name=${heroName}`).then(function(jsonData) {
+    fetchJsonData(buildApiUrl("characters") + `&name=${heroName}`).then(function (jsonData) {
 
         // Main variables from Marvel API - v1/public/characters
         let data = jsonData.data;
-        let result = data.results[0];
-        let thumbnailUrl = `${result.thumbnail.path}.${result.thumbnail.extension}`;
-        let comics = data.results[0].comics;
-        let officialUrls = data.results[0].urls;
-        let officialDetail = officialUrls[0];
-        let officialWiki = officialUrls[1];
-        
+
+
         // Aaron :) you can add the data paths in here to get larger images, info, comic, etc. You can also append/
         // add classes to your html here. E.g (in jQuery - $(<"insert id/class">).addClass("thumbnailSize") )
 
         if (data.total !== 0) {
 
-            console.log(thumbnailUrl);
-            console.log(result.name);
-            console.log(result.description);
-            console.log(comics);
-            console.log(officialUrls);
-            console.log(officialDetail);
-            console.log(officialWiki);
-
             showHeroCards(data);
         } else {
-            return console.error("No heroes found!")};
-        
-    }).catch(function(err) {
+            return console.error("No heroes found!")
+        }
+        ;
+
+    }).catch(function (err) {
         console.log(err);
         console.log("Failed to get hero data!");
     })
@@ -96,21 +85,25 @@ function heroLocator(event) {
 // };
 
 // Separate function to show hero cards
-function showHeroCards(data) {
+function showHeroCards(hero) {
 
     let heroCardContainer = document.getElementById("heroCardContainer");
 
-    data.forEach((hero) => {
+    let result = hero.results[0];
+    let thumbnailUrl = `${result.thumbnail.path}.${result.thumbnail.extension}`;
+    let officialUrls = result.urls;
+    let officialDetail = officialUrls[0];
+    let officialWiki = officialUrls[1];
+    let heroImage = thumbnailUrl;
+    let heroName = result.name;
+    let heroBio = result.description;
+    let heroDetail = officialDetail.url;
+    let heroWiki = officialWiki.url;
 
-        let heroImage = thumbnailUrl;
-        let heroName = data.results.name;
-        let heroBio = data.results.description;
-        let heroDetail = officialDetail.url;
-        let heroWiki = officialWiki.url;
+    // construct hero card layout
 
-        // construct hero card layout
-
-        const heroCard = `
+    const heroCard = `
+        <h3>HERO LOCATIONS FOUND</h3>
         <div class="heroCard"> 
         <img src=${heroImage} alt="This is an image of ${heroName}" />
         <h4>${heroName}</h4>
@@ -122,18 +115,21 @@ function showHeroCards(data) {
         </div>
         `;
 
-        // append herocard to container
-        heroCardContainer.innerHTML += heroCard;
-    });
-
+    // append herocard to container
+    heroCardContainer.innerHTML += heroCard;
 };
+
+
+
 
 // Search input
 
 // Autocomplete widget for search bar
 function fetchJsonData(url) {
-    return fetch(url).then(function(response) {
-        if (response.ok) return response.json();
+    return fetch(url).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
         return Promise.reject(response);
     });
 }
@@ -157,11 +153,11 @@ function buildHeroList() {
     let allHeroList = [];
 
     // This function handles the response for one request of up to 100 results.
-    let handleResponse = function(jsonData) {
+    let handleResponse = function (jsonData) {
         let data = jsonData.data;
 
         // Add each hero name into our list.
-        data.results.forEach(function(result) {
+        data.results.forEach(function (result) {
             allHeroList.push(result.name);
         });
 
@@ -174,10 +170,10 @@ function buildHeroList() {
     };
 
     // Start requesting chunks of data
-    fetchJsonData(characterApiUrl).then(handleResponse).then(function() {
+    fetchJsonData(characterApiUrl).then(handleResponse).then(function () {
         console.log(`Got list of ${allHeroList.length} heroes, copy following line into heroes.js to update the list`);
         console.log(JSON.stringify(allHeroList));
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.log("Failed to build hero list: " + err);
     });
 }
